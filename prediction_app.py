f159c9a226ce6e9f9138b964d9745a8faf127e20
Jannat-Raza <108749549+Jannat-Raza@users.noreply.github.com>
2024-12-05 10:42:29 +0500

    Update prediction_app.py

--- prediction_app.py
+++ prediction_app.py
@@ -1,4 +1,11 @@
-
+import os
+import numpy as np
+import cv2
+import streamlit as st
+from sklearn.metrics.pairwise import cosine_similarity
+from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import Model
 
 # Load the VGG16 model
 base_model = VGG16(weights='imagenet')
